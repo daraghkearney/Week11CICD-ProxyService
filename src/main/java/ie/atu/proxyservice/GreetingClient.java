01f8548254531a9b34d68f4000bfe18edb226098
DARAGHKEARNEY-STUDEN <g00438215@atu.ie>
2025-12-03 15:58:57 +0000

    Added GreetingClient

---
+++ src/main/java/ie/atu/proxyservice/GreetingClient.java
@@ -0,0 +1,15 @@
+package ie.atu.proxyservice;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(
+        name = "greetingClient",
+        url = "${greeting.service.base-url}"
+)
+
+public interface GreetingClient {
+    @GetMapping("/api/greet/{name}")
+    String call_greet(@PathVariable String name);
+}
